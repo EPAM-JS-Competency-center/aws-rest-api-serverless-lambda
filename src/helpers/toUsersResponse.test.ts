590fb1b8bffda3a5f1ddfef6e9a4c8cd65b4c1e7
Vadim Tcaregorodtcev <dev.tcaregorodtcev@gmail.com>
2021-09-17 15:24:34 +0300

    fix comments

--- src/helpers/toUsersResponse.test.ts
+++ src/helpers/toUsersResponse.test.ts
@@ -1,6 +1,6 @@
 import { toUsersResponse } from "./toUsersResponse";
 
-describe("Campaigns Controller Response", () => {
+describe("Users Controller Response", () => {
   it("should create controller response", async () => {
     const result = toUsersResponse([
       {
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,53 @@
-const path = require('path');
-const slsw = require('serverless-webpack');
-const TerserPlugin = require('terser-webpack-plugin');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const path = require("path");
+const slsw = require("serverless-webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
-const folderName = './.webpack/';
+const folderName = "./.webpack/";
 
 module.exports = {
   entry: slsw.lib.entries,
   resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.ts',
-      '.tsx',
-    ],
-    plugins: [
-      new TsconfigPathsPlugin()
-    ]
+    extensions: [".js", ".json", ".ts", ".tsx"],
+    plugins: [new TsconfigPathsPlugin()],
   },
   output: {
-    libraryTarget: 'commonjs',
+    libraryTarget: "commonjs",
     path: path.join(__dirname, `${folderName}`),
-    filename: '[name].js',
+    filename: "[name].js",
   },
-  mode: 'production',
+  mode: "production",
   optimization: {
     minimize: true,
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           keep_classnames: true,
-          keep_fnames: true
+          keep_fnames: true,
         },
         parallel: true,
-      })
+      }),
     ],
   },
-  externals: [{
-    'aws-sdk': 'commonjs aws-sdk',
-  }],
-  target: 'node',
-  devtool: 'source-map',
+  externals: [
+    {
+      "aws-sdk": "commonjs aws-sdk",
+    },
+  ],
+  target: "node",
+  devtool: "source-map",
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
-        include: path.resolve(__dirname, 'src'),
+        include: path.resolve(__dirname, "src"),
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
               compilerOptions: {
-                module: 'es6',
-                target: 'es2017',
+                module: "es6",
+                target: "es2019",
               },
               transpileOnly: true,
             },
